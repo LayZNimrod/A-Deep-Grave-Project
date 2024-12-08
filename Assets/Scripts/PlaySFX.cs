b1abca6908a89ea44efaa503c42e4151b40efc4b
Kro <124914389+Krowno04@users.noreply.github.com>
2024-12-08 18:45:03 -0500

    SFX

--- Assets/Scripts/PlaySFX.cs
+++ Assets/Scripts/PlaySFX.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,16 @@ public class PlaySFX : MonoBehaviour
 {
     public AudioSource audioSource;
 
+    [Header("CameraShake Settings")]
+    [SerializeField] private CineMachineShake cameraShake;
+    [SerializeField] private float shakeIntensity = 1f;
+    [SerializeField] private float shakeTimer = 1f;
+    [SerializeField] private float StartFrequency = 1f;
+    [SerializeField] private float EndFrequency = 3f;
+
+    public float speed = 1;
+
+    [SerializeField] private CinemachineVirtualCamera cam;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +30,22 @@ public class PlaySFX : MonoBehaviour
             if (audioSource != null)
             {
                 audioSource.Play();
+                StartCoroutine(ZoomOut());
+                cameraShake.ShakeCamera(shakeIntensity, shakeTimer, StartFrequency, EndFrequency);
             }
             this.GetComponent<BoxCollider2D>().enabled = false;
         }
     }
 
+    IEnumerator ZoomOut()
+    {
+        for (int i = 0; i <= 12; i++)
+            if (cam.m_Lens.OrthographicSize <= 9)
+            {
+                cam.m_Lens.OrthographicSize += 0.5f;
+                yield return new WaitForSeconds(0.2f);
+            }
+    }
     // Update is called once per frame
     void Update()
     {
