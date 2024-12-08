876edd5135d1cff67cee0d048fcc6ba2ca137895
Kro <124914389+Krowno04@users.noreply.github.com>
2024-12-07 22:47:21 -0500

    WIP on eye

--- Assets/Scripts/PlayCutscene.cs
+++ Assets/Scripts/PlayCutscene.cs
@@ -11,10 +11,13 @@ public class PlayCutscene : MonoBehaviour
     [SerializeField] private float shakeIntensity = 1f;
     [SerializeField] private float shakeTimer = 1f;
 
+    public GameObject ToAnimate;
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        ToAnimate.GetComponent<Animator>().Play("EyeClosed");
     }
 
     private void OnTriggerEnter2D(Collider2D player)
@@ -23,10 +26,17 @@ public class PlayCutscene : MonoBehaviour
         {
             audioSource.Play();
             cameraShake.ShakeCamera(shakeIntensity, shakeTimer);
+            AnimateEye();
         }
         this.GetComponent<BoxCollider2D>().enabled = false;
     }
 
+    IEnumerator AnimateEye()
+    {
+        ToAnimate.GetComponent<Animator>().Play("EyeAnim");
+        yield return new WaitForSeconds(15);
+        ToAnimate.GetComponent<Animator>().Play("EyeOpen ");
+    }
     // Update is called once per frame
     void Update()
     {
