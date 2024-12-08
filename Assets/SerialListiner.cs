135891cfb3fa94fed6f23c1af4c74cdfc5da7ce8
LayZNimrod <2zachmiller@gmail.com>
2024-12-07 21:34:34 -0500

    got communication working

---
+++ Assets/SerialListiner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerialListiner : MonoBehaviour
+{
+    [SerializeField] SerialController se;
+    public int sensor_val = 0;
+    public int buttonR = 0;
+    public int buttonL = 0;
+
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        
+    }
+    void OnMessageArrived(string msg)
+    {
+        string[] msgArray = msg.Split(',');
+        sensor_val = int.Parse(msgArray[0]);
+        buttonR = int.Parse(msgArray[1]);
+        buttonL = int.Parse(msgArray[2]);
+
+        
+        serialOutput();
+    }
+
+    void OnConnectionEvent(bool success)
+    {
+        if (success)
+            Debug.Log("Connection established");
+        else
+            Debug.Log("Connection attempt failed or disconnection detected");
+    }
+    public void serialOutput()
+    {
+        if (true)
+        {
+            se.SendSerialMessage("2");
+        }
+        else if (false)
+        {
+            se.SendSerialMessage("1");
+        }
+        else
+        {
+            se.SendSerialMessage("0");
+        }
+    }
+}
